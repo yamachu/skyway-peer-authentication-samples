8ba61baf2b81d30beb13633391d7dcb736370836
yamachu <yamachu.dev@gmail.com>
2020-05-04 00:06:59 +0900

    cors対応

--- nodejs/sample.js
+++ nodejs/sample.js
@@ -21,10 +21,18 @@ const credentialTTL = 3600; // 1 hour
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
   next();
+  }
 });
 
 app.post('/authenticate', (req, res) => {
